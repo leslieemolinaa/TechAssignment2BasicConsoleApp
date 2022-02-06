fb1105d78d97b3d4f47b5feb459cbd0a12c70b4e
emman <emman@DESKTOP-88MACNP>
2022-02-06 12:19:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechAssignment2BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
